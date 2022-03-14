e54f93fea3146f5b25622a44dcc858c6af3caaec
codeThinh <codethinh@gmail.com>
2022-03-13 18:09:36 -0700

    added location search

--- js/scripts.js
+++ js/scripts.js
@@ -51,3 +51,35 @@ function modalCenter() {
         'align-items': 'center'
     })
 }
+
+
+// Location Input
+const locationCardTemplate = document.querySelector("[data-location-template]")
+const locationCardContainer = document.querySelector("[data-location-cards-container]")
+const searchInput = document.querySelector("[data-search]")
+
+let locations = []
+
+searchInput.addEventListener("input", x => {
+    const value = x.target.value.toLowerCase()
+    locations.forEach(location => {
+    const isVisible =
+        location.company.toLowerCase().includes(value) ||
+        location.city.toLowerCase().includes(value)
+    location.element.classList.toggle("hide", !isVisible)
+    })
+})
+
+fetch("https://jsonplaceholder.typicode.com/users")
+    .then(res => res.json())
+    .then(data => {
+    locations = data.map(location => {
+        const card = locationCardTemplate.content.cloneNode(true).children[0]
+        const header = card.querySelector("[data-header]")
+        const body = card.querySelector("[data-body]")
+        header.textContent = location.company.name
+        body.textContent = location.address.street + ", " + location.address.city
+        locationCardContainer.append(card)
+        return { company: location.company.name, city: location.address.street + ", " + location.address.city, element: card }
+    })
+    })
